41c5e2a6752bab20167a50c2bd378f3d513a46af
Aawal92 <abdulawal3092@gmail.com>
2023-06-04 09:24:22 -0400

    LowestNumber math problem modified

--- src/math_problems/LowestNumber.java
+++ src/math_problems/LowestNumber.java
@@ -11,7 +11,16 @@ public class LowestNumber {
         int[] array = new int[] {211, 110, 99, 34, 67, 89, 67, 456, 321, 456, 78, 90, 45, 32, 56, 78, 90, 54, 32, 123,
                 67, 5, 679, 54, 32, 65};
 
+        System.out.println(lowestNum(array));
+    }
+    public static int lowestNum(int [] array){
+        int smallest = array[0];
 
+        for(int i = 0; i < array.length; i++){
+            if(array[i] < smallest)
+                smallest = array[i];
+        }
+        return smallest;
     }
 
 }
