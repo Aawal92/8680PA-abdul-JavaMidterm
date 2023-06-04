6757b89c0247e83bfff17d10d7e039fb3d2d241a
Aawal92 <abdulawal3092@gmail.com>
2023-06-04 09:35:44 -0400

    Factorial modified

--- src/math_problems/Factorial.java
+++ src/math_problems/Factorial.java
@@ -11,7 +11,16 @@ public class Factorial {
      */
 
     public static void main(String[] args) {
+        System.out.println(Factorial(5));
 
+    }
+    public static int Factorial(int n){
+        if(n > 1) {
+            return n * Factorial(n - 1);
+        }
+        else {
+            return 1;
+        }
 
     }
 
