e5e7d1af3bd95113eb5444f7a35ab1e2966601be
Aawal92 <abdulawal3092@gmail.com>
2023-06-04 09:23:01 -0400

    PrimeNumber modified

--- src/math_problems/PrimeNumber.java
+++ src/math_problems/PrimeNumber.java
@@ -1,5 +1,8 @@
 package math_problems;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrimeNumber {
 
     /** INSTRUCTIONS
@@ -12,6 +15,48 @@ public class PrimeNumber {
 
     public static void main(String[] args) {
 
+        /*
+        List<Integer> primeNumbers = new ArrayList<>();
+        int number = 10;
+
+        for(int i = 2; i <= number; i++){
+            boolean isPrime = true;
+
+            for(int j = 2; j <= number /2; j++){
+                if(i % j == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime){
+                primeNumbers.add(i);
+            }
+        }
+        System.out.println("The prime numbers are : ");
+        for(int primeNumber : primeNumbers){
+            System.out.println(primeNumber);
+        }
+
+         */
+
+        int number = 1000000;
+
+        boolean isPrime = true;
+
+        for(int i = 2; i <= number; i++){
+            int sqrt = (int) Math.sqrt(i);
+
+            for(int j = 2; j <= sqrt; j++){
+                if(i % j == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime){
+                System.out.println(i);
+            }
+            isPrime = true;
+        }
     }
 
 }
