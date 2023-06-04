75ff95c781e7f32dfc633eca1d7ec9f2d0ded75e
Aawal92 <abdulawal3092@gmail.com>
2023-06-04 09:39:21 -0400

    UseArrayList modified

--- src/data_structures/UseArrayList.java
+++ src/data_structures/UseArrayList.java
@@ -1,5 +1,10 @@
 package data_structures;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class UseArrayList {
 
     /** INSTRUCTIONS
@@ -11,6 +16,37 @@ public class UseArrayList {
      */
 
     public static void main(String[] args) {
+
+        ArrayList<String> states = new ArrayList<String>();
+        states.add("PA");
+        states.add("NYC");
+        states.add("NJ");
+        states.add("BOSTON");
+        states.add("LA");
+        states.remove("NJ");
+
+        System.out.println(states.get(0));
+
+        // retrieve using For - Each loop
+//        for(int i = 0; i < states.size(); i++){
+//            System.out.println(states.get(i));
+//        }
+
+        // retrieve using while loop
+        int count = 0;
+        while(states.size() > count){
+            System.out.println(states.get(count));
+            count++;
+        }
+
+
+        // retrieve using inhance for loop
+//        for(String state : states){
+//            System.out.println(state);
+//        }
+
+
+
 
     }
 
