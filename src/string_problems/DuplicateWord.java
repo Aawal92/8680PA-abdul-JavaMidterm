1a77e338c0a0acf5e1b30b57d8c7f21cb80cde2a
Aawal92 <abdulawal3092@gmail.com>
2023-06-04 09:00:37 -0400

    modified duplicateWord file

--- src/string_problems/DuplicateWord.java
+++ src/string_problems/DuplicateWord.java
@@ -1,5 +1,9 @@
 package string_problems;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class DuplicateWord {
 
     /** INSTRUCTIONS
@@ -11,6 +15,26 @@ public class DuplicateWord {
         String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
 
         // Implement here
+        String[] words = st.split(" ");
+
+        Map<String, Integer> sw = new HashMap<>();
+
+        for(String word : words){
+            if(sw.get(word) == null){
+                sw.put(word, 1);
+            }else{
+                sw.put(word, sw.get(word) + 1);
+            }
+        }
+        Iterator<String> itr = sw.keySet().iterator();
+        while (itr.hasNext()){
+            String temp = itr.next();
+
+            if(sw.get(temp) > 1){
+                System.out.println(temp + " : " + sw.get(temp));
+            }
+
+        }
 
     }
 
