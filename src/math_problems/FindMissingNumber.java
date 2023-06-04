2e886b4d89acac6d17736c833b77197f55f81e98
Aawal92 <abdulawal3092@gmail.com>
2023-06-04 09:32:52 -0400

    FindMissingNumber modified

--- src/math_problems/FindMissingNumber.java
+++ src/math_problems/FindMissingNumber.java
@@ -9,5 +9,17 @@ public class FindMissingNumber {
     public static void main(String[] args) {
         int[] array = new int[] {10, 2, 1, 4, 5, 3, 7, 8, 6};
 
+        System.out.println(missingNum(array,10 ));
+
+    }
+
+    public static int missingNum(int [] array, int n){
+        int sumOfArray = n * (n + 1)/ 2;
+
+        int addArray = 0;
+        for(int i = 0; i < n-1; i++){
+            addArray += array[i];
+        }
+        return  sumOfArray - addArray;
     }
 }
