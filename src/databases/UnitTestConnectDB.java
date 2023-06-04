5b8b4dcd5b63d6c9f73839870366c8ab5c7fa20c
Aawal92 <abdulawal3092@gmail.com>
2023-06-04 09:37:15 -0400

    UnitTestConnectDB modified

--- src/databases/UnitTestConnectDB.java
+++ src/databases/UnitTestConnectDB.java
@@ -18,15 +18,15 @@ public class UnitTestConnectDB {
         SharedStepsDatabase ssdb = new SharedStepsDatabase();
 
         // region ExecuteQueryReadOne
-//        String query = "SELECT * FROM EMPLOYEES.EMPLOYEES LIMIT 10";
+//        String query = "select * from sakila.actor";
 //        String result = ssdb.executeQueryReadOne(query);
 //
 //        System.out.println(result);
-        // endregion
-
-        // region executeQueryReadAllSingleColumn
-//        String queryTwo = "SELECT * FROM EMPLOYEES.EMPLOYEES LIMIT 10";
-//        List<String> results = ssdb.executeQueryReadAllSingleColumn(queryTwo, "last_name");
+//        // endregion
+//
+//        // region executeQueryReadAllSingleColumn
+//        String queryTwo = "select * from sakila.actor;";
+//        List<String> results = ssdb.executeQueryReadAllSingleColumn(queryTwo, "last_update");
 //
 //        for (String s : results) {
 //            System.out.println(s);
@@ -35,9 +35,8 @@ public class UnitTestConnectDB {
         // endregion
 
         // region executeQueryReadAll
-//        String queryThree = "SELECT * FROM EMPLOYEES.EMPLOYEES E " +
-//                       "INNER JOIN EMPLOYEES.DEPT_EMP DE ON E.EMP_NO = DE.EMP_NO " +
-//                       "WHERE E.FIRST_NAME LIKE 'Alain'";
+//        String queryThree = "SELECT city.Name , countrylanguage.Language FROM city INNER JOIN" +
+//                " countrylanguage ON city.CountryCode = countrylanguage.CountryCode";
 //
 //        List<List<String>> data = ssdb.executeQueryReadAll(queryThree);
 //
@@ -75,21 +74,21 @@ public class UnitTestConnectDB {
         // endregion
 
         // region insertMap
-//        HashMap<Object, Object> map = new HashMap<>();
-//        map.put("Student1", 7934);
-//        map.put("Student2", 6319);
-//
-//        ssdb.insertMap("test_insert_map", map);
-//
-//        String query = "SELECT * FROM TEST_INSERT_MAP";
-//        List<List<String>> results = ssdb.executeQueryReadAll(query);
-//
-//        for (List<String> row : results) {
-//            for (String cell : row) {
-//                System.out.print(cell + "\t\t");
-//            }
-//            System.out.println();
-//        }
+        HashMap<Object, Object> map = new HashMap<>();
+        map.put("Student1", 7934);
+        map.put("Student2", 6319);
+
+        ssdb.insertMap("test_insert_map", map);
+
+        String query = "SELECT * FROM TEST_INSERT_MAP";
+        List<List<String>> results = ssdb.executeQueryReadAll(query);
+
+        for (List<String> row : results) {
+            for (String cell : row) {
+                System.out.print(cell + "\t\t");
+            }
+            System.out.println();
+        }
 
         // endregion
 
