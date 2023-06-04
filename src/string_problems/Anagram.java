3a8b7d70bae2d692303fbe1a750706a29587d433
Aawal92 <abdulawal3092@gmail.com>
2023-06-04 09:03:00 -0400

    modified Anagram

--- src/string_problems/Anagram.java
+++ src/string_problems/Anagram.java
@@ -2,6 +2,8 @@ package string_problems;
 
 public class Anagram {
 
+
+
     /** INSTRUCTIONS
      * Write a Java Program, `isAnagram`, to check if any two strings are anagrams
      *
@@ -14,5 +16,40 @@ public class Anagram {
      */
 
     // Implement Here
+    public static void main(String[] args){
+        System.out.println(isAnagram("CAT", "ACT"));
+    }
+    public static boolean isAnagram (String first_sentence, String second_sentence){
+        int first_length = first_sentence.length();
+        int second_length = second_sentence.length();
+
+        if(first_length != second_length){
+            return false;
+        }
+
+        first_sentence = first_sentence.toLowerCase();
+        second_sentence = second_sentence.toLowerCase();
+
+        int[] char_frequencies = new int[26];
+
+        for(int i = 0; i < first_length; i++){
+            char current_char = first_sentence.charAt(i);
+            int index = current_char - 'a';
+            char_frequencies[index]++;
+        }
+
+        for(int i = 0; i < second_length; i++){
+            char current_char = second_sentence.charAt(i);
+            int index = current_char - 'a';
+            char_frequencies[index]--;
+        }
+
+        for(int i = 0; i < 26; i++){
+            if(char_frequencies[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
 
 }
